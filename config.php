96a62ec07f10a2efa1997b5cd4afd499f9e1579f
12VinodAcharya <achabinod1112@gmail.com>
2023-03-23 09:15:39 +0545

    MySQL database config added

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_NAME', 'snippets');
+define('DB_NAME', 'FormData');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'Binod010#');
 define('DB_HOST', 'localhost');
